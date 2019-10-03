e99a37cfb065d3d976f72bb95cb50c692247f824
Gero <geronimo@devsar.com>
2019-10-03 10:32:45 -0300

    Corrigo bottombar para estilo IG

--- src/components/BottomBar.vue
+++ src/components/BottomBar.vue
@@ -19,31 +19,34 @@
       <about/>
     </ion-tab>
 
+    <ion-tab tab="likes">
+    </ion-tab>
+
     <!-- Use v-slot:bottom with Vue ^2.6.0 -->
     <template slot="bottom">
       <ion-tab-bar>
         <ion-tab-button tab="scroll">
-          <ion-icon name="list"></ion-icon>
-          <ion-label>Scroll</ion-label>
+          <ion-icon name="home"></ion-icon>
           <!-- <ion-badge>0</ion-badge> -->
         </ion-tab-button>
 
         <!-- Provide a custom route to navigate to -->
         <ion-tab-button tab="notificaciones">
-          <ion-icon name="notifications"></ion-icon>
-          <ion-label>Notificaciones</ion-label>
+          <ion-icon name="search"></ion-icon>
         </ion-tab-button>
 
         <!-- Provide extra data to route -->
         <ion-tab-button tab="map">
-          <ion-icon name="map"></ion-icon>
-          <ion-label>Map</ion-label>
+          <ion-icon name="add-circle-outline"></ion-icon>
+        </ion-tab-button>
+
+        <ion-tab-button tab="likes">
+          <ion-icon name="heart-empty"></ion-icon>
         </ion-tab-button>
 
         <!-- Provide custom click handler -->
         <ion-tab-button tab="about">
-          <ion-icon name="information-circle"></ion-icon>
-          <ion-label>About</ion-label>
+          <ion-icon name="contact"></ion-icon>
         </ion-tab-button>
       </ion-tab-bar>
     </template>
@@ -69,5 +72,14 @@ export default {
 </script>
 
 <style>
-
+ion-tab-button{
+  --color: #000;
+  --color-activated: #000;
+}
+ion-icon{
+  color: #000;
+}
+.tab-selected ion-icon{
+  color: #222;
+}
 </style>
